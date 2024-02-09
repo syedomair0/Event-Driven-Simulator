7d30e3c5d1548246aa7ac98b3c10ba7bb1ac40a1
sn0wcr@sh <syedomair0@gmail.com>
2024-02-09 14:03:38 -0600

    changed simulator to core

---
+++ core.py
@@ -0,0 +1,37 @@
+class Node():
+    def __init__(self):
+        pass
+
+class Link():
+    def __init__(self, node1, node2):
+        pass
+
+class Packet():
+    def __init__(self):
+        pass
+
+
+class Network():
+    def __init__(self, links: list()):
+        pass
+
+class Event():
+    def __init__(self):
+        pass
+
+class BirthEvent(Event):
+    def __init__(self):
+        pass
+
+class DeathEvent(Event):
+    def __init__(self):
+        pass
+
+class MonitorEvent(Event):
+    def __init__(self):
+        pass
+
+class EventQueue():
+    def __init__(self):
+        pass
+
